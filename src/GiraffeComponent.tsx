310494d23a7f36d6023b5d98db82dd585c9a04d6
Sean Cheatham <seancheatham@gmail.com>
2024-09-17 21:51:46 -0400

    Linter fix

--- src/GiraffeComponent.tsx
+++ src/GiraffeComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import * as g from "@giraffechain/giraffe-sdk";
 import GridComponent from './GridComponent';
 
